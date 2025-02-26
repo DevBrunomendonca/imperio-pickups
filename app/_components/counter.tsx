621028a9220c60d555d36681d0729328775f3d6e
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-02-26 16:26:05 -0300

    feat: add component Counter

---
+++ app/_components/counter.tsx
@@ -0,0 +1,15 @@
+import {
+  CouterPros,
+  useAnimationCounter,
+} from "../_hooks/use-animation-statistics";
+
+const Counter = ({ endValue, start, className }: CouterPros) => {
+  const { count } = useAnimationCounter({ endValue, start });
+  return (
+    <span className={`${className} text-black text-7xl font-bold`}>
+      +{count}
+    </span>
+  );
+};
+
+export default Counter;

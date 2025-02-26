67ce3f2c775b3572a7bdfebc9a64ebbb54d98bb5
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-02-26 20:44:07 -0300

    feat: add data images gallery slides

---
+++ app/_data/data-images-gallery.ts
@@ -0,0 +1,13 @@
+interface dataImagesGalleryProps {
+  srcImage: string;
+}
+
+const dataImagesGallery: dataImagesGalleryProps[] = [
+  { srcImage: "/images/imagem-pickup-2.webp" },
+  { srcImage: "/images/imagem-pickup-3.webp" },
+  { srcImage: "/images/imagem-pickup-4.webp" },
+  { srcImage: "/images/imagem-pickup-5.webp" },
+  { srcImage: "/images/imagem-pickup-6.webp" },
+];
+
+export default dataImagesGallery;

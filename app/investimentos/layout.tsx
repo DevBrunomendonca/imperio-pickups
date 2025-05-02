c5190d9f1168eefa80e925bc226bd18008f18d36
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-05-01 22:48:52 -0300

    feat: add script tag

--- app/investimentos/layout.tsx
+++ app/investimentos/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 import "../globals.css";
 
 import { register } from "swiper/element/bundle";
@@ -8,6 +9,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import HeaderInvestments from "./_components/header-investimenrs";
 import FooterInvestments from "./_components/footer-investments";
+import Image from "next/image";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -27,7 +29,35 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="pt-br">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1366903341221843');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </Head>
       <body className={`${poppins.className}`}>
+        <noscript>
+          <Image
+            height={1}
+            width={1}
+            alt=""
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1366903341221843&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <HeaderInvestments />
         {children}
         <FooterInvestments />
